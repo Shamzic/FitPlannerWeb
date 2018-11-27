fc29e24f6bed4f9c8c637eceeddf80fd4952545b
Shamzic <shamery@free.fr>
2018-11-27 14:08:14 -0500

    update frond profil/edit

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -19,10 +19,10 @@ import gql from 'graphql-tag'
       email
     }
   }
-` 
+`
 */
 const UPDATE_USER_MUTATION = gql`
-  mutation UpdateUserMutation($data: UserUpdateInput!, $where: UserWhereUniqueInput!) { 
+  mutation UpdateUserMutation($data: UserUpdateInput!, $where: UserWhereUniqueInput!) {
     updateUser(data: $data, where: $where) {
 	  id
       name
@@ -30,8 +30,8 @@ const UPDATE_USER_MUTATION = gql`
     }
   }
 `
-// 
-  
+//
+
 ///
 
 const USER_QUERY = gql`
@@ -51,7 +51,7 @@ const USER_QUERY = gql`
     "name" : name ,
   },
   "where": {"id":'cjonjhuq0jllf0a64864l79sr'}
-  
+
 }}//, ,{id}{data},{where2}
 		  //variables={{data}, {where}}//{name,email }
 		  //onCompleted={data => this._confirm(data)}
@@ -80,13 +80,16 @@ class Edit extends Component {
 	console.log("ssssssssssssssssssssssaaaaaaaaaaaaaaaaaaallllllllllllluuuuuuuuutttt")
     return (
 	 <div>
+     <div className="container text-center" id="main">
+       <div class="row justify-content-md-center">
+         <div className="col col-lg-4">
 	  <Query query={USER_QUERY}>
         {({ loading, error, data }) => {
           if (loading)
             return <div>Fetching</div>
           if (error)
 			return <div>Error</div>
-		
+
 		  const dataUser = data.user
 		  //this.setState( {where : {id: dataUser.id}})
 		  //userid=dataUser.id
@@ -94,12 +97,11 @@ class Edit extends Component {
 		  //where:{id : dataUser.id }
           return (
 		  //state={name,email}
-	
-    
-				<div key={dataUser.id} className="">
-				
-					<p>   name :
 
+
+				<div key={dataUser.id} className="">
+
+					<p><strong> Name :</strong>
 					<input
                   className="form-control"
                   value={this.state.name}
@@ -109,11 +111,11 @@ class Edit extends Component {
                 />
 					</p>
 
-					<p>   email :
+					<p> <strong>  Email :</strong>
 						<input
                   className="form-control"
                   value={this.state.email}
-                  onChange={e => this.setState({ email: e.target.value})} 
+                  onChange={e => this.setState({ email: e.target.value})}
                   type="text"
                   placeholder = {dataUser.email}
                 />
@@ -122,11 +124,11 @@ class Edit extends Component {
 				</div>
           )
 
-		  
+
 
         }}
       </Query>
-	  
+
 	  <Mutation
           mutation={UPDATE_USER_MUTATION}
           variables={{
@@ -135,7 +137,7 @@ class Edit extends Component {
     "name" : this.state.name ,
   },
   "where": {"id":'cjonjhuq0jllf0a64864l79sr'}
-  
+
 }}//, ,{id}{data},{where2}
 		  //variables={{data}, {where}}//{name,email }
 		  onCompleted={data => this._confirm(data)}
@@ -145,10 +147,15 @@ class Edit extends Component {
 		  onClick={updateUserMutation}//(data => this._confirm(data))
 		  >Save </button>}
     </Mutation>
-	  
-	  
-	  
-	 </div>
+
+
+
+
+   </div>
+   </div>
+   </div>
+   </div>
+
     )
   }
 //<MutationUpdateUser name={this.state.name} email={this.state.email}    />
@@ -160,16 +167,16 @@ class Edit extends Component {
     //this._saveUserData(token)
     this.props.history.push(`/profile`)
 	window.location.reload()
-	
+
   }
-  
+
   _saveUserData = token => {
     localStorage.setItem(AUTH_TOKEN, token)
-	
+
   }
 
 }
-  
 
-  
+
+
 export default Edit
